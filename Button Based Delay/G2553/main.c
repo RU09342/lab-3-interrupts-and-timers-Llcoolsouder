edc740561fe727a81c8657ff0acd829dce02897c
Lonnie Souder <souderl9@students.rowan.edu>
2017-10-09 17:41:41 -0400

    BtnDelay: Tested button circuit (preparing to use capture on P1.1)

--- Button Based Delay/G2553/main.c
+++ Button Based Delay/G2553/main.c
@@ -12,28 +12,41 @@ enum Edge {Rising_Edge, Falling_Edge};
 
 int main(void)
 {
-	WDTCTL = WDTPW | WDTHOLD;				// stop watchdog timer
+    // NOTE:    Registers are cleared on power up
+    //          This means bits need only to be set; not cleared.
+
+	WDTCTL = WDTPW | WDTHOLD;		// stop watchdog timer
 	
-	P1SEL &= ~BIT0;							// Set P1.0 to GPIO
-	P1SEL2 &= ~BIT0;
-	P1DIR |= BIT0;							// Set P1.0 (LED) to output
-	P1OUT &= ~BIT0;							// Initialize P1.0 to LOW
+	//P1SEL &= ~(BIT1 + BIT0);		// Set P1.0 and P1.1 to GPIO
+	//P1SEL2 &= ~(BIT1 + BIT0);
+	P1DIR |= BIT0;					// Set P1.0 (LED) to output
+	//P1OUT &= ~BIT0;			    // Initialize P1.0 to LOW
+	P1REN |= BIT1;                  // Enable resistor on P1.1
+	//P1OUT &= ~BIT1;               // Set pull down resistor on P1.1
+	P1IE |= BIT1;                   //enable interrupt on P1.1
+	//P1IES &= ~BIT1                // Interrupt on posedge of P1.1
 
-	// NOTE: 	Timer A registers are cleared on power up
-	//			This means bits need only to be set; not cleared.
+	TACTL |=    TASSEL_2 +          // Select clock source, clk divider, clk mode, overflow interrupt
+	            ID_1 +
+	            MC_1 +
+	            TAIV;
+	TACCR0 = 0x61A8;				// Set clock period to 32000/8000000 [s] (10Hz)
+	TACCTL0 |= CCIE;				// Enable Timer A capture/compare interrupt
 
-	TACTL |= TASSEL_2 + ID_1 + MC_1;		// Select clock source, clk divider, clk mode
-	TACCR0 = 0x61A8;						// Set clock period to 32000/8000000 [s] (10Hz)
-	TACCTL0 |= CCIE + SCS;						// Enable Timer A capture/compare interrupt
-
-	_BIS_SR(LPM1_bits + GIE);				// Enter Low Power Mode 1 and enable global interrupt
+	_BIS_SR(LPM1_bits + GIE);		// Enter Low Power Mode 1 and enable global interrupt
 
 	return 0;
 }
 
-//Timer Interrupt Service Routine
+// Timer Interrupt Service Routine
 #pragma vector = TIMER0_A0_VECTOR
 __interrupt void Timer_A(void) {
-	P1OUT ^= BIT0; 						// Toggle P1.0 (LED)
+	P1OUT ^= BIT0; 					// Toggle P1.0 (LED)
 	TACCTL0 &= ~CCIFG;
+}
+
+// P1.1 (Button) Service routine
+#pragma vector = PORT1_VECTOR
+__interrupt void PORT_1(void) {
+    TACCR0 = 0x0000;
 }
