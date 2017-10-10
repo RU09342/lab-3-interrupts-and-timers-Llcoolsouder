25342eb1a33934bdd62f5718511b2178e4ef432b
Lonnie Souder <souderl9@students.rowan.edu>
2017-10-09 20:13:54 -0400

    Finished BtnDelay for G2553

--- Button Based Delay/G2553/main.c
+++ Button Based Delay/G2553/main.c
@@ -8,45 +8,70 @@
  *      Author: Lonnie Souder II
  */
 
-enum Edge {Rising_Edge, Falling_Edge};
+
+long int overflows;	// Keeps track of how many times TA overflows while Btn is pushed
+long int count;	// Keeps track of how long its been since the LED has been toggled
+
+enum Trigger {Rising_Edge, Falling_Edge};
+enum Trigger edge = Falling_Edge;	//Keeps track of Btn pushes
 
 int main(void)
 {
-    // NOTE:    Registers are cleared on power up
+    // NOTE:    Most registers are cleared on power up
     //          This means bits need only to be set; not cleared.
 
 	WDTCTL = WDTPW | WDTHOLD;		// stop watchdog timer
 	
-	//P1SEL &= ~(BIT1 + BIT0);		// Set P1.0 and P1.1 to GPIO
-	//P1SEL2 &= ~(BIT1 + BIT0);
+	//P1SEL &= ~(BIT2 | BIT0);		// Set P1.0 and P1.2 to GPIO
+	//P1SEL2 &= ~(BIT2 | BIT0);
 	P1DIR |= BIT0;					// Set P1.0 (LED) to output
-	//P1OUT &= ~BIT0;			    // Initialize P1.0 to LOW
-	P1REN |= BIT1;                  // Enable resistor on P1.1
-	//P1OUT &= ~BIT1;               // Set pull down resistor on P1.1
-	P1IE |= BIT1;                   //enable interrupt on P1.1
-	//P1IES &= ~BIT1                // Interrupt on posedge of P1.1
+	P1OUT &= ~BIT0;			        // Initialize P1.0 to LOW
+	P1REN |= BIT2;                  // Enable resistor on P1.2
+	//P1OUT &= ~BIT2;               // Set pull down resistor on P1.2
+	P1IE |= BIT2;                   // enable interrupt on P1.2
+	P1IES &= ~BIT2;                 // Interrupt on posedge of P1.2
 
-	TACTL |=    TASSEL_2 +          // Select clock source, clk divider, clk mode, overflow interrupt
-	            ID_1 +
-	            MC_1 +
-	            TAIV;
+	TACTL |=    TASSEL_2 |          // Select clock source, clk divider, clk mode, overflow interrupt
+	            ID_1 |
+	            MC_1;
+
 	TACCR0 = 0x61A8;				// Set clock period to 32000/8000000 [s] (10Hz)
-	TACCTL0 |= CCIE;				// Enable Timer A capture/compare interrupt
 
-	_BIS_SR(LPM1_bits + GIE);		// Enter Low Power Mode 1 and enable global interrupt
+	TACCTL0 |= CCIE;				// Enable Timer A capture/compare interrupt 0
+
+	_BIS_SR(LPM1_bits | GIE);		// Enter Low Power Mode 1 and enable global interrupt
 
 	return 0;
 }
 
-// Timer Interrupt Service Routine
+// Timer0 Interrupt Service Routine
 #pragma vector = TIMER0_A0_VECTOR
 __interrupt void Timer_A(void) {
-	P1OUT ^= BIT0; 					// Toggle P1.0 (LED)
+	if (edge == Rising_Edge){		// Btn is no longer pushed
+		if (!(P1IN & BIT2)){
+			edge = Falling_Edge;
+			count = 0;
+		}
+		else {						// Count overflows while button is pushed
+			overflows++;
+		}
+	}
+	else {							// Button is no longer pushed
+		if (count == overflows){	// Count equals the previous number of overflows
+			P1OUT ^= BIT0;
+			count = 0;
+		}
+		else
+			count++;
+	}
 	TACCTL0 &= ~CCIFG;
 }
 
-// P1.1 (Button) Service routine
+// P1.2 (Button) Service routine
 #pragma vector = PORT1_VECTOR
 __interrupt void PORT_1(void) {
-    TACCR0 = 0x0000;
+    overflows = 0;
+    edge = Rising_Edge;
+    P1IFG &= ~BIT2;
 }
+
